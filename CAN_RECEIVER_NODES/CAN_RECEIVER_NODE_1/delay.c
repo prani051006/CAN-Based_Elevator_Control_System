6a0ae45c32aff9a4366783444cca2a4b2340da5a
prani051006 <139366272+prani051006@users.noreply.github.com>
2026-04-13 21:13:11 +0530

    Add files via upload

---
+++ CAN_RECEIVER_NODES/CAN_RECEIVER_NODE_1/delay.c
@@ -0,0 +1,20 @@
+#include <LPC21xx.H>
+#include "header.h"
+
+void delay_sec(unsigned int sec)
+{
+	T0PR=60000000-1;
+	T0TCR=0x1;
+	while(T0TC<sec);
+	T0TCR=0x03;
+	T0TCR = 0x00;
+}
+
+void delay_ms(unsigned int ms)
+{
+	T0PR=60000-1;
+	T0TCR=0x1;				   
+	while(T0TC<ms);
+	T0TCR=0x03;
+	T0TCR = 0x00;
+}
---
+++ CAN_RECEIVER_NODES/CAN_RECEIVER_NODE_1/header.h
@@ -0,0 +1,31 @@
+#ifndef __HEADER_H_
+#define __HEADER_H_
+typedef unsigned int u32;
+
+
+void delay_sec(unsigned int sec);
+void delay_ms(unsigned int ms);
+
+//UART DRIVER FUNCTION DECLARATION
+void uart1_init(unsigned int baud);
+void uart1_tx(unsigned char data);
+unsigned char uart1_rx(void);
+void uart1_tx_string(char *ptr);
+void uart1_tx_integer(int num);
+void uart1_tx_hex(int num);
+
+
+typedef struct DATAFRAME
+{
+	u32 id;
+	u32 rtr;
+	u32 dlc;
+	u32 byteA;
+	u32 byteB;
+}CAN2_DF;
+
+void can2_init(void);
+void can2_tx(CAN2_DF m1);
+void can2_rx(CAN2_DF *m1);
+
+#endif
