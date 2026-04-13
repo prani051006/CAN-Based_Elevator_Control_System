7009926f25c71239b49019eb8e388102a9939c64
prani051006 <139366272+prani051006@users.noreply.github.com>
2026-04-13 21:13:56 +0530

    Add files via upload

---
+++ CAN_RECEIVER_NODES/CAN_RECEIVER_NODE_2/maincode.c
@@ -0,0 +1,50 @@
+#include<lpc21xx.h>
+#include "header.h"
+#define LED 1<<4
+int main()
+{
+	CAN2_DF rm1;
+	IODIR0|=LED;
+	IOSET0|=LED;
+	can2_init();
+	uart1_init(115200);
+	uart1_tx_string("nodeB TESTING CAN\r\n");
+
+	  while(1)
+	  {		
+		can2_rx(&rm1);
+		if(rm1.rtr==0){
+			uart1_tx_string("nodeB: DATA-FRAME is received\r\n");
+			uart1_tx_hex(rm1.id);
+			uart1_tx(' ');
+			uart1_tx_hex(rm1.dlc);
+			uart1_tx(' ');
+			uart1_tx_hex(rm1.byteA);
+			uart1_tx(' ');
+			uart1_tx_hex(rm1.byteB);
+			uart1_tx_string("\r\n");
+		}
+			
+		else
+		{
+			uart1_tx_string("nodeB: REMOTE-FRAME is received\r\n");
+			uart1_tx_hex(rm1.id);
+			uart1_tx(' ');
+			uart1_tx_hex(rm1.dlc);
+			uart1_tx(' ');
+			uart1_tx_string("\r\n");
+
+		}
+		if(rm1.byteA==2)
+		{
+			IOCLR0=LED;
+			delay_sec(2);
+			IOSET0=LED;
+		}	
+
+	}
+
+
+	
+
+}
---
+++ CAN_RECEIVER_NODES/CAN_RECEIVER_NODE_2/uart0_driver.c
@@ -0,0 +1,91 @@
+#include <LPC21xx.H>
+#include<stdio.h>
+
+#include "header.h"
+
+
+void uart1_init(unsigned int baud)
+{
+
+	PINSEL0|=1<<16|1<<18;
+
+	U1LCR=0x83;
+
+	switch(baud){	
+
+		case 115200:U1DLL=32;	break;//PCLK=60MHz
+
+		case 921600:U1DLL=4;	break;
+
+		default:	U1DLL=32	;	
+
+	}
+
+	U1LCR=0x03;
+
+}
+
+
+void uart1_tx(unsigned char data)
+{
+
+	U1THR=data;
+
+	while((U1LSR&(1<<5))==0);
+
+}
+
+
+unsigned char uart1_rx(void)
+{
+
+	while((U1LSR&0x1)==0);
+
+	return U1RBR;
+
+}
+
+
+
+
+void uart1_tx_string(char *ptr)
+{
+
+	while(*ptr!=0)
+	{
+
+		U1THR=*ptr;
+
+		while((U1LSR&(1<<5))==0);
+
+		ptr++;
+
+	}
+
+}
+
+
+void uart1_tx_integer(int num)
+{
+
+	char buf[10];
+
+	sprintf(buf,"%d",num);
+
+	uart1_tx_string(buf);
+
+}
+
+
+
+
+void uart1_tx_hex(int num)
+{
+
+	char buf[10];
+
+	sprintf(buf,"%X",num);
+
+	uart1_tx_string(buf);
+
+}
